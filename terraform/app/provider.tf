6182a8161615f85a658691c3fc003cf9568f7b05
rakesh <rakeshreddy6576@gmail.com>
2024-08-15 22:18:10 +0530

    fixing varibaleset

--- terraform/app/provider.tf
+++ terraform/app/provider.tf
@@ -10,7 +10,7 @@ terraform {
 # Configure the AWS Provider
 
 provider "aws" {
-  region     = us-east-1
+  region     = var.aws_region
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
--- terraform/app/variable.tf
+++ terraform/app/variable.tf
@@ -13,10 +13,10 @@
 #   description = "AWS region to be set via the TF_VAR enviornment variable"
 # }
 
-# variable "aws_region" {
-#   type    = string
-#   default = "us-east-1"
-# }
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
 
 # variable "aws_access_key" {
 #   type = map(number)
