0354658f93b11ce6e795e82faacf65cb437967f8
rakesh <rakeshreddy6576@gmail.com>
2024-08-15 17:59:05 +0530

    fixxxx

--- terraform/app/variable.tf
+++ terraform/app/variable.tf
@@ -1,10 +1,10 @@
 variable "aws_access_key" {
-  type    = map(string)
+  type    = string
   description = "access key"
 }
 
 variable "aws_secret_key" {
-  type    = map(string)
+  type    = string
   description = "secret key"
 }
 
