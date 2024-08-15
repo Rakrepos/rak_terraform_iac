af37fad46335378796c3a4d8a9ab8a884e831e50
rakesh <rakeshreddy6576@gmail.com>
2024-08-15 17:09:30 +0530

    updated terraform files

--- terraform/app/variable.tf
+++ terraform/app/variable.tf
@@ -1,3 +1,7 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" {
+ sensitive = true
+}
+variable "aws_secret_key" {
+ sensitive = true
+}
 variable "aws_region" {}
