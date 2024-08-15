8fde487d966231ebb5c3d53c98e9f6b0bfd46e47
rakesh <rakeshreddy6576@gmail.com>
2024-08-15 17:27:01 +0530

    string addedd

--- terraform/app/variable.tf
+++ terraform/app/variable.tf
@@ -1,13 +1,16 @@
 variable "aws_access_key" {
  default = {}
  sensitive = true
+ type = list(string)
 }
 variable "aws_secret_key" {
  default = {}
  sensitive = true
+ type = list(string)
 }
 variable "aws_region" {
   default = {}
   sensitive = false
+  type = list(string)
   description = "AWS region to be set via the TF_VAR enviornment variable"
 }
