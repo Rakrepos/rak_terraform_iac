69ea68ec651c7f907f0230f87c56fd681faccdcf
rakesh <rakeshreddy6576@gmail.com>
2024-08-15 22:25:27 +0530

    final code

--- terraform/app/provider.tf
+++ terraform/app/provider.tf
@@ -5,6 +5,8 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+ required_version = ">= 1.1.5"
 }
 
 # Configure the AWS Provider
@@ -14,27 +16,3 @@ provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
-
-
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-# terraform {
-
-#   cloud {
-#     organization = "<ORGANIZATION_NAME>"
-
-#     workspaces {
-#       name = "learn-terraform-variable-sets-dev"
-#     }
-#   }
-
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 3.63"
-#     }
-#   }
-
-#   required_version = ">= 1.1.0"
-# }
--- terraform/app/variable.tf
+++ terraform/app/variable.tf
@@ -1,32 +1,7 @@
-# variable "aws_access_key" {
-#   type    = string
-#   description = "access key"
-# }
-
-# variable "aws_secret_key" {
-#   type    = string
-#   description = "secret key"
-# }
-
-# variable "aws_region" {
-#   type = map(string)
-#   description = "AWS region to be set via the TF_VAR enviornment variable"
-# }
-
 variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
-
-# variable "aws_access_key" {
-#   type = map(number)
-#   # default = ""
-# }
-
-# variable "aws_secret_key" {
-#   type = map(number)
-#   # default = ""
-# }
 
 variable "AWS_SECRET_ACCESS_KEY" {
   default = ""
