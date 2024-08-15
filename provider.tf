5374161c2d258f7224a9a6ae7cfe0b74e055edee
rakesh <rakeshreddy6576@gmail.com>
2024-08-15 15:18:11 +0530

    updated tf

--- provider.tf
+++ provider.tf
@@ -10,6 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+  access_key = "AKIA2RD7UI3H47X2EK2R"
+  secret_key = "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"
 }
